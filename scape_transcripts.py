8e653759f73fb53340a4913f6a2d9d95672d49a6
Ronan Diver <ronand1997@gmail.com>
2024-07-20 21:51:09 +0100

    Add transcript parser

--- scape_transcripts.py
+++ scape_transcripts.py
@@ -1,6 +1,5 @@
 # file to scrape podcast transcripts from https://zoe.com/learn/category/podcasts
 
-
 import logging
 
 import requests
@@ -8,10 +7,21 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 from typing import Optional
 
+
+class PodcastTranscript:
+    def __init__(self, title: str, transcript: str):
+        self.title = title
+        self.transcript = transcript
+
+
 class TranscriptScraper:
 
-    long_podcast_base_url = "https://zoe.com/learn/category/podcasts/zoe-podcast-long-episodes"
-    short_podcast_base_url = "https://zoe.com/learn/category/podcasts/zoe-podcast-shorts"
+    long_podcast_base_url = (
+        "https://zoe.com/learn/category/podcasts/zoe-podcast-long-episodes"
+    )
+    short_podcast_base_url = (
+        "https://zoe.com/learn/category/podcasts/zoe-podcast-shorts"
+    )
 
     def __init__(self, logger: Optional[logging.Logger] = None):
         self.logger = logger if logger else logging.getLogger(__name__)
@@ -34,7 +44,6 @@ class TranscriptScraper:
         else:
             raise ValueError("long_or_short must be either 'long' or 'short'")
 
-
         podcast_urls = []
 
         # define the base url and subsequent pages to scrape
@@ -49,15 +58,41 @@ class TranscriptScraper:
             soup = BeautifulSoup(requests.get(url).content, "html.parser")
             for link in soup.find_all("a"):
                 candidate = link.get("href")
-                if candidate and candidate.startswith("/learn") and not (candidate.startswith("/learn/category") or candidate == "/learn"):
+                if (
+                    candidate
+                    and candidate.startswith("/learn")
+                    and not (
+                        candidate.startswith("/learn/category") or candidate == "/learn"
+                    )
+                ):
                     podcast_urls.append(candidate)
                     self.logger.debug(f"Found long podcast url: {candidate}")
 
         return [base_url_for_article + url for url in podcast_urls]
-    
 
+    def _get_transcript(self, url: str) -> PodcastTranscript:
+        """
+        Get the transcript of the podcast from the given url
+        """
+        soup = BeautifulSoup(requests.get(url).content, "html.parser")
+        # all transcripts start at a point in the page where it says "Transcript" as a header
+        # and end at the next header
+        transcript = ""
+        for header in soup.find_all("h2"):
+            if header.text.lower() == "transcript":
+                for sibling in header.next_siblings:
+                    if sibling.name == "h2":
+                        break
+                    if sibling.name == "p":
+                        transcript += sibling.text + " "
+        return PodcastTranscript(soup.title.text, transcript)
 
 
 if __name__ == "__main__":
     test = TranscriptScraper()
-    print(test._get_url_of_podcasts("long"))
+    # print(test._get_url_of_podcasts("long"))
+    ts = test._get_transcript(
+        "https://zoe.com/learn/podcast-ai-and-personalized-healthcare"
+    )
+    print(ts.title)
+    # print(ts.transcript)
